c3c3b12260e441347644e10e6df24586a8094809
chmiprojects <153160073+chmiprojects@users.noreply.github.com>
2024-05-25 14:39:24 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,36 @@
-import subprocess
-import sys
+import os
 
-def install_package(package_name):
-    try:
-        result = subprocess.run([sys.executable, '-m', 'pip', 'install', package_name], check=True, capture_output=True, text=True)
-        print(f"Package {package_name} installed successfully")
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Failed to install package {package_name}")
-        print(e.stdout)
-        print(e.stderr)
+# Path to rc.local
+rc_local_path = "/etc/rc.local"
 
-packages = ['customtkinter', 'RPi.GPIO', 'ultralytics', 'pigpio', 'CTkMessagebox']
+# Lines to add
+lines_to_add = [
+    "sudo chown root.gpio /dev/gpiomem &",
+    "sudo chmod g+rw /dev/gpiomem &",
+    "sudo pigpiod &"
+]
 
-for package in packages:
-    install_package(package)
+# Read the current rc.local content
+with open(rc_local_path, "r") as file:
+    content = file.readlines()
 
+# Check if each line to add already exists
+lines_to_append = [line + "\n" for line in lines_to_add if line + "\n" not in content]
+
+# If there are lines to append
+if lines_to_append:
+    # Find the index of "exit 0" line
+    exit_index = next((i for i, line in enumerate(content) if line.strip() == "exit 0"), None)
+    if exit_index is not None:
+        # Insert the new lines before "exit 0"
+        content = content[:exit_index] + lines_to_append + content[exit_index:]
+
+        # Write the modified content back to rc.local
+        with open(rc_local_path, "w") as file:
+            file.writelines(content)
+
+        print("New lines added to rc.local.")
+    else:
+        print("Couldn't find 'exit 0' line in rc.local. Lines not added.")
+else:
+    print("Lines already exist in rc.local. No changes needed.")
